359035c1c884184684fee3d7f7c9c4e4abf59d07
gamer765 <gameplayer765@yahoo.com>
2026-09-08 13:02:39 -0400

    Bump strict obfuscation experiment to exp2

--- source/app/build.gradle.kts
+++ source/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tn.loukious.facebookappadsremover"
         minSdk = 30
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.10.0-obfexp1"
+        versionCode = 17
+        versionName = "1.10.0-obfexp2-strict"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
