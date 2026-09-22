6bd11c8c0a9421c2a934d390bbb3f1f2a896f1a6
gamer765 <gameplayer765@yahoo.com>
2026-09-22 17:34:47 -0400

    Bump obfexp11 after similar-Reels wrapper rollback

--- source/app/build.gradle.kts
+++ source/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tn.loukious.facebookappadsremover"
         minSdk = 30
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.10.0-obfexp10-fb579-reels"
+        versionCode = 26
+        versionName = "1.10.0-obfexp11-fb579-reels"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
