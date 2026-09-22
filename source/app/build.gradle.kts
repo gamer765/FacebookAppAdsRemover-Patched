ebe4cbc103f98c330dbf515cd8767743141536b0
gamer765 <gameplayer765@yahoo.com>
2026-09-22 16:24:06 -0400

    Bump universal experiment to obfexp8

--- source/app/build.gradle.kts
+++ source/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tn.loukious.facebookappadsremover"
         minSdk = 30
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.10.0-obfexp7-fb579-reels"
+        versionCode = 23
+        versionName = "1.10.0-obfexp8-fb579-reels"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
