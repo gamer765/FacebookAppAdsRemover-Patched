98a70f4ab4b3fbd119483960e17b4c3073bcffd5
gamer765 <gameplayer765@yahoo.com>
2026-09-24 00:29:36 -0400

    Bump obfexp19 conditional Reels state fix

--- source/app/build.gradle.kts
+++ source/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tn.loukious.facebookappadsremover"
         minSdk = 30
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.10.0-obfexp18-fb579-prestatefix"
+        versionCode = 34
+        versionName = "1.10.0-obfexp19-fb579-conditionalstatefix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
